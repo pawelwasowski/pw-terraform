f9785f8ab31b6a3ee0db57f65a2dc9971336f6d3
pawelwasowski <pawel.wasowski@coderylabs.com>
2021-09-28 13:08:22 +0200

    fix formatting

--- providers.tf
+++ providers.tf
@@ -7,7 +7,7 @@ provider "aws" {
 }
 # This provider is used to create resource that must be strictly in eu-east-1 region, e.g. cloudfront certificates
 provider "aws" {
-  alias = "us-east-1"
+  alias  = "us-east-1"
   region = "us-east-1"
 
   assume_role {
