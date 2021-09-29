10a8d7479c231fa568eb01ecfd1b75c9890d87fb
pawelwasowski <pawel.wasowski@coderylabs.com>
2021-09-29 07:51:12 +0200

    update

---
+++ backend/remote-backend-config-dev.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket = "e-mobility-terraform-remote-state"
+    key = "e-mobility"
+    region = "eu-central-1"
+  }
+  required_providers {
+    aws      = "~> 3.59.0"
+    null     = "~> 3.0.0"
+    random   = "~> 3.0.0"
+    template = "~> 2.2.0"
+    tls      = "~> 3.0.0"
+  }
+  required_version = ">= 1.0.7"
+}
