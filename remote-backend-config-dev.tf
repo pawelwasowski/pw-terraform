db26e7cf2f5143607f9220a3ee5007b03ba1687f
pawelwasowski <pawel.wasowski@coderylabs.com>
2021-09-29 07:38:39 +0200

    update

--- remote-backend-config-dev.tf
+++ remote-backend-config-dev.tf
@@ -1,16 +1,16 @@
 terraform {
   backend "s3" {
-    bucket = "e-mobility-terraform-remote-state"
-    key = "e-mobility"
-    region = "eu-central-1"
+    bucket   = "e-mobility-terraform-remote-state"
+    key      = "e-mobility"
+    region   = "eu-central-1"
     role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/e-mobility-terraform-state-manager"
   }
   required_providers {
-    aws = "~> 3.59.0"
-    null = "~> 3.0.0"
-    random = "~> 3.0.0"
+    aws      = "~> 3.59.0"
+    null     = "~> 3.0.0"
+    random   = "~> 3.0.0"
     template = "~> 2.2.0"
-    tls = "~> 3.0.0"
+    tls      = "~> 3.0.0"
   }
   required_version = ">= 1.0.7"
 }
