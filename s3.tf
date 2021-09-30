1286f020d99e12d47b484d1d4a22688bec8c0f2e
pawelwasowski <pawel.wasowski@coderylabs.com>
2021-09-30 15:01:44 +0200

    enable concurrency

--- s3.tf
+++ s3.tf
@@ -2,11 +2,4 @@ resource "aws_s3_bucket" "test" {
   bucket = "${var.product}-${var.stage}-test"
   acl    = "private"
 }
-resource "aws_s3_bucket" "test2" {
-  bucket = "${var.product}-${var.stage}-test2"
-  acl    = "private"
-}
-resource "aws_s3_bucket" "test3" {
-  bucket = "${var.product}-${var.stage}-test3"
-  acl    = "private"
-}
+
