77668810840980814c1ca29c18336ff7f643ef93
pawelwasowski <pawel.wasowski@coderylabs.com>
2021-09-30 15:03:29 +0200

    add s3 bucket test3

--- s3.tf
+++ s3.tf
@@ -6,3 +6,7 @@ resource "aws_s3_bucket" "test2" {
   bucket = "${var.product}-${var.stage}-test2"
   acl    = "private"
 }
+resource "aws_s3_bucket" "test3" {
+  bucket = "${var.product}-${var.stage}-test3"
+  acl    = "private"
+}
