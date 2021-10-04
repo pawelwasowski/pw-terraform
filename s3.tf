68753829795ab3e95ad1d81f7a743ee1aa7d1992
pawelwasowski <pawel.wasowski@coderylabs.com>
2021-10-04 12:28:08 +0200

    aws task force demo

--- s3.tf
+++ s3.tf
@@ -10,3 +10,7 @@ resource "aws_s3_bucket" "test3" {
   bucket = "${var.product}-${var.stage}-test3"
   acl    = "private"
 }
+resource "aws_s3_bucket" "test4" {
+  bucket = "${var.product}-${var.stage}-test4"
+  acl    = "private"
+}
